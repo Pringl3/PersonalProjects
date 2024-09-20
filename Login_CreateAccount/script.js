5c769fb79fd51bed160765919363a0fff381bcec
Pringl3 <77543249+Pringl3@users.noreply.github.com>
2024-09-20 13:05:06 +0100

    Update script.js

---
+++ Login_CreateAccount/script.js
@@ -0,0 +1,12 @@
+// Login/SignUp Slider
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.toggle("active");
+})
+
+loginBtn.addEventListener('click', () => {
+    container.classList.toggle("active");
+})
